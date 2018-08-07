0658ab3fe0b065669452fec70a086966c94f0d79
Peaceman <muhammedcy@gmail.com>
2018-08-07 18:39:10 +0300

    Section 9

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MVC.swift
+//  breakpoint
+//
+//  Created by My Macbook Pro on 07/08/2018.
+//  Copyright © 2018 My Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+}
