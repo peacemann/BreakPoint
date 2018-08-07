0749b1c227b8cff8194c1106355604bfb5ee7e8f
Peaceman <muhammedcy@gmail.com>
2018-08-07 20:04:37 +0300

    Section 11

--- breakpoint/Controller/CreatePostVC.swift
+++ breakpoint/Controller/CreatePostVC.swift
@@ -18,7 +18,7 @@ class CreatePostVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.delegate = self
-
+        sendBtn.bindToKeyboard()
         // Do any additional setup after loading the view.
     }
     
