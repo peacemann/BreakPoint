1848c45735eb8eccd355d8ae979ad1bcafd98a5f
Peaceman <muhammedcy@gmail.com>
2018-08-11 13:00:59 +0300

    Section 25

--- breakpoint/Controller/GroupFeedVC.swift
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -56,7 +56,7 @@ class GroupFeedVC: UIViewController {
     
 
     @IBAction func backBtnWasPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissDetail()
     }
 
     @IBAction func sendBtnWasPressed(_ sender: Any) {
--- breakpoint/Controller/GroupsVC.swift
+++ breakpoint/Controller/GroupsVC.swift
@@ -56,7 +56,7 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
         guard let groupFeedVC = storyboard?.instantiateViewController(withIdentifier: "GroupFeedVC") as? GroupFeedVC else { return }
         
         groupFeedVC.initData(forGroup: groupsArray[indexPath.row])
-        present(groupFeedVC, animated: true, completion: nil)
+        presentDetail(groupFeedVC)
     }
 }
 
